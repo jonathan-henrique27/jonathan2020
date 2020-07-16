2c774a3a32b899a1c26b77658cc9060d6720312c
jonathan-henrique27 <jonathahenriquesilva36@gmail.com>
2020-07-16 18:14:37 -0300

    CRUD Scaffolding

---
+++ SalesWebMvc2/Data/SalesWebMvc2Context.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace SalesWebMvc2.Models
+{
+    public class SalesWebMvc2Context : DbContext
+    {
+        public SalesWebMvc2Context (DbContextOptions<SalesWebMvc2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SalesWebMvc2.Models.Department> Department { get; set; }
+    }
+}
